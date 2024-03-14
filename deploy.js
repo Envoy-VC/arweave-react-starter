b9dbc36a7fdefc0b66c90497a309674c1a3aae5a
Vedant Chainani <vedantchainani1084@gmail.com>
2024-03-14 23:26:03 +0530

    chore: update deploy script

--- deploy.js
+++ deploy.js
@@ -1,14 +1,28 @@
 import Irys from '@irys/sdk';
+
 import * as dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
+dotenv.config();
 
 import fs from 'fs';
 import path from 'path';
 
-function getTotalSize(folderPath) {
-  let stats = fs.readdir(folderPath);
-  return stats.size;
-}
+const getDirSize = (dirPath) => {
+  let size = 0;
+  const files = fs.readdirSync(dirPath);
+
+  for (let i = 0; i < files.length; i++) {
+    const filePath = path.join(dirPath, files[i]);
+    const stats = fs.statSync(filePath);
+
+    if (stats.isFile()) {
+      size += stats.size;
+    } else if (stats.isDirectory()) {
+      size += getDirSize(filePath);
+    }
+  }
+
+  return size;
+};
 
 const deploy = async () => {
   const url = 'devnet';
@@ -18,31 +32,31 @@ const deploy = async () => {
   const irys = new Irys({
     url,
     token,
-    key: process.env.PK,
+    key: process.env.WALLET_PK,
     config: { providerUrl },
   });
   await irys.ready();
   const folderToUpload = './dist';
   const balance = irys.utils.fromAtomic(await irys.getLoadedBalance());
   console.log('Current Balance: ', balance);
-  const cost = irys.utils.fromAtomic(
-    await irys.getPrice(getTotalSize(folderToUpload))
-  );
+  let size = getDirSize(folderToUpload);
+  console.log('Current size: ', size);
+  const cost = irys.utils.fromAtomic(await irys.getPrice(size));
   console.log('Cost to Upload: ', cost);
-  // try {
-  // 	const receipt = await irys.uploadFolder(folderToUpload, {
-  // 		indexFile: 'index.html',
-  // 	});
-  // 	console.log(
-  // 		`Files uploaded. Manifest ID: https:gateway.irys.xyz/tx/${receipt.id}`
-  // 	);
-  // 	const balanceAfterUpload = irys.utils.fromAtomic(
-  // 		await irys.getLoadedBalance()
-  // 	);
-  // 	console.log('Balance after upload: ', balanceAfterUpload);
-  // } catch (e) {
-  // 	console.log('Error uploading file ', e);
-  // }
+  try {
+    const receipt = await irys.uploadFolder(folderToUpload, {
+      indexFile: 'index.html',
+    });
+    console.log(
+      `Files uploaded. Manifest ID: https:gateway.irys.xyz/tx/${receipt.id}`
+    );
+    const balanceAfterUpload = irys.utils.fromAtomic(
+      await irys.getLoadedBalance()
+    );
+    console.log('Balance after upload: ', balanceAfterUpload);
+  } catch (e) {
+    console.log('Error uploading file ', e);
+  }
 };
 
 deploy().catch((err) => {
